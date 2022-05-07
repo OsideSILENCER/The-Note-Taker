d6131d9a47e4f41502d32228c6de6a4b9e6e76c3
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-05-06 22:43:56 -0700

    add all files and changes

--- helper/server.js
+++ helper/server.js
@@ -1,17 +1,13 @@
-const express = require("express");
-const fs = require("fs");
-const path = require('path');
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-require('./routes/route.js')(app);
-
-// Setup listener
-app.listen(PORT, function() {
-    console.log("App listening on: http://localhost" + PORT);
-});  
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
